8b35f1f273ef4249eec4321372fa678bd33d82a9
Simon Spedsbjerg <simon@spedsbjerg.dk>
2022-07-02 17:48:41 +0200

    Add project files.

---
+++ SimonSpedsbjerg/Classes/Repo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimonSpedsbjerg.Classes {
+    public class Repo {
+        public string Name { get { return name; } }
+        public string Url { get { return url; } }
+        private string name;
+        private string url;
+
+        public Repo(string name, string url) {
+            this.name = name;
+            this.url = url;
+            }
+        }
+    }
