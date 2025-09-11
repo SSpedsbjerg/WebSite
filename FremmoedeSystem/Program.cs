9cd442a862205e36cdf0db818cd514e29420e0b4
Simon Spedsbjerg <simon@spedsbjerg.dk>
2025-09-12 01:50:26 +0200

    first encryption

--- FremmoedeSystem/Program.cs
+++ FremmoedeSystem/Program.cs
@@ -2,13 +2,29 @@ using FremmødeSystem.Components;
 
 class Program {
     private static void Main(string[] args) {
+        Console.WriteLine(args);
         var builder = WebApplication.CreateBuilder(args);
-        // Add services to the container.
+
         builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+        string? certPath = "";
+        string password = "";
+        if(!args.Contains("--no-ssl")) {
+            certPath = builder.Configuration["Kestrel:Endpoints:Https:Certificate:Path"];
+            password = File.ReadAllText("/run/secrets/cert_password.txt").Trim();
+        }
+
+        if(!args.Contains("--no-ssl")) {
+            builder.WebHost.ConfigureKestrel(options => {
+                options.ConfigureHttpsDefaults(https => {
+                    https.ServerCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2(certPath, password);
+                });
+            });
+        }
+
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if(!app.Environment.IsDevelopment()) {
             app.UseExceptionHandler("/Error", createScopeForErrors: true);
         }
